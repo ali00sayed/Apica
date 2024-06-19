be86603ff86ae602adc8feea2e11253365963e1b
Ali Sayed <88398843+ali00sayed@users.noreply.github.com>
2024-06-19 13:09:54 +0530

    Update Header.jsx
    Change the project name from Apica to Search Fusion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center space-x-5">
-        <h1 className="text-xl">Apica</h1>
+        <h1 className="text-xl">Search Fusion</h1>
       </div>
 
       <div className="flex space-x-7 cursor-pointer">
